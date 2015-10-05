6ed640d2559492be19ae61404bdd42eda5eb6c1e
Farrah Brink <febrink@gmail.com>
2015-10-04 21:01:00 -0400

    Fill in contact page.

--- app/assets/javascripts/core_pages.js
+++ app/assets/javascripts/core_pages.js
@@ -1,2 +1,29 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function() {
+    setMap('284 Main St. Coopersville MI');
+});
+
+function setMap(address) {
+  
+    var geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({ address: address }, function(results, status) {
+        
+        var center = results[0].geometry.location;
+    
+        var lat = center.lat();
+        var lng = center.lng();
+        
+        var mapOptions = {
+            center: new google.maps.LatLng(lat, lng),
+            zoom: 11,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        
+        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        
+        var marker = new google.maps.Marker({
+            map: map,
+            position: center
+        });
+    });
+}
